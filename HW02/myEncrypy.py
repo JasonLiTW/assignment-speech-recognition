4e28a05a8cfb61e31bff29656e2c333029600d91
JASON-PC\Jason <jason860421@gmail.com>
2018-03-13 20:52:34 +0800

    HW02 & HW03

---
+++ HW02/myEncrypy.py
@@ -0,0 +1,23 @@
+import scipy.io.wavfile as wav
+import matplotlib.pyplot as plt
+import numpy as np
+# B10417022 李政瑩
+rate, signal = wav.read('./hello.wav') # hello.wav只有單聲道
+sigSize = np.size(signal)
+time = np.linspace(0, sigSize, sigSize) / rate
+
+encrypt = np.copy(signal)
+for i in range(sigSize):
+    if signal[i] > 0:
+        encrypt[i] = 1 - signal[i]
+    elif signal[i] < 0:
+        encrypt[i] = -1 - signal[i]
+encrypt = np.flipud(encrypt)
+
+plt.subplot(2, 1, 1)
+plt.plot(time, signal)
+plt.title("Original Signal")
+plt.subplot(2, 1, 2)
+plt.plot(time, encrypt)
+plt.title("After Encrypt")
+plt.show()
---
+++ HW03/hw03.py
@@ -0,0 +1,25 @@
+import scipy.io.wavfile as wav
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+B10417022 李政瑩.
+hide.wav有雙聲道，但兩個聲道資料似乎是一樣的。
+另外pdf圖上也只有畫一個聲道，但是x軸時間卻是兩倍(約3.5秒)，有點奇怪?
+
+這邊因為兩個聲道都一樣，畫出來會重疊，所以下面只取第一個聲道來畫。
+"""
+
+rate, signal = wav.read('./hide.wav')
+signal = np.asarray([data[0] for data in signal]) # 取第一個聲道
+sigSize = np.size(signal)
+time = np.linspace(0, sigSize, sigSize) / rate
+normal = signal.astype(float, copy=True) / 32768
+
+plt.subplot(2, 1, 1)
+plt.plot(time, signal)
+plt.title("Original Signal")
+plt.subplot(2, 1, 2)
+plt.plot(time, normal)
+plt.title("After Normalization")
+plt.show()
